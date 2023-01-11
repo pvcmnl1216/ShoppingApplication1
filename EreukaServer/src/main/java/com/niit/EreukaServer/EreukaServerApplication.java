5b5f12fce900e21c6a616b6d2b0e503067591bd5
Philip Meshach <Philipmeshach.pm@gmail.com>
2023-01-11 13:40:57 +0530

    completed

--- EreukaServer/src/main/java/com/niit/EreukaServer/EreukaServerApplication.java
+++ EreukaServer/src/main/java/com/niit/EreukaServer/EreukaServerApplication.java
@@ -2,15 +2,13 @@ package com.niit.EreukaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EreukaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EreukaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EreukaServerApplication.class, args);
+    }
 }
